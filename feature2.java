f750457d03144fb6af2bc7414d2fc6ecd0c0a212
ronitdalal27 <dalalronit131@gmail.com>
2024-11-26 19:26:10 +0530

    first project

---
+++ feature2.java
@@ -0,0 +1,6 @@
+public class feature2 {
+    public static void main(String[] args) {
+        System.out.println("Bye Ronit");
+    }
+    
+}
---
+++ feature3.java
@@ -0,0 +1,5 @@
+public class feature3 {
+    public static void main(String[] args) {
+        System.out.println("How are you Ronit ?");
+    }
+}
